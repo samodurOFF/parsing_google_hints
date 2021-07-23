99ef9c972921e2cfb1b9f2303e496b828501e415
samodurOFF <samodurovdanil@gmail.com>
2021-07-24 01:42:44 +0500

    without recursion

--- get_hints.py
+++ get_hints.py
@@ -1,111 +1,64 @@
 import os
-import sys
 import pandas as pd
-
 from bs4 import BeautifulSoup
 import requests
 
+with open('phrases_for_suggest.txt', 'r', encoding='utf-8') as file:
+    phrases = file.read().split('\n')
 
-def filtration(phrases: list, base_word: str, index: int = 0) -> list:
-    assert len(base_word.split(' ')) != 1  # проверка на короткую фразу
+file_1 = 'suggest.csv'
+file_2 = 'suggest.txt'
 
-    if index == len(phrases):  # услование выхода из рекурсии
-        return phrases
+if os.path.exists(file_1):
+    os.remove(file_1)
 
-    phrase = phrases[index]  # ьекущая фраза по рекурсии
+if os.path.exists(file_2):
+    os.remove(file_2)
 
-    if base_word == phrase:  # если базовая фраза равна текущей фразе или не содержиться в ней
-        phrases.remove(phrase)  # то удаляем текущую фразу из списка
-        return filtration(phrases, base_word)  # и вызываем функцию снова без изменения индекса
-    else:  # если базовая фраза не содержиться в текущей фразе, то нужно вызвать функцию перейдя к следующей фразе
-        return filtration(phrases, base_word, index + 1)
+passes = input(
+    'Укажите максимальное количество проходов нажмите enter если не хотите устанавливать этот параметр): ')
 
+pd.DataFrame(columns=['PHRASE', 'SUGGESTION']).to_csv(file_1, index=False, sep=';')
+new_phrases = []
+all_phrases = []
+i = 1
+while True:
+    if phrases == new_phrases:
+        break
 
-def find_suggestions(phrases, final_dict: dict):
-    """
-    The function for looking for and record Google search suggestion
-
-    :param phrases: object with phrases that are needed to look for
-    :return:
-    """
-
-    if len(phrases) == 0:  # условие выхода из первой рекурсии
-        return final_dict
+    elif passes != '' and i > int(passes):
+        break
 
     else:
-        phrase = phrases.pop(0)
-        url = f'http://clients1.google.com/complete/search?output=toolbar&num=100&hl=en&pq=suggestqueries.google.com&cp=1&q={phrase}'
-        page = requests.get(url)
-        soup = BeautifulSoup(page.text, 'html.parser')
-        suggestion_list = []
-        for i in soup.find_all('suggestion'):
-            suggestion_list.append(i['data'])
-
-        else:
-            final_dict[phrase] = ', '.join(filtration(suggestion_list, phrase))
-            return find_suggestions(phrases, final_dict)
-
-
-if __name__ == '__main__':
-    with open('phrases_for_suggest.txt', 'r', encoding='utf-8') as file:
-        phrases = file.read().split('\n')
-
-    file_1 = 'suggest.csv'
-    file_2 = 'suggest.txt'
-
-    if os.path.exists(file_1):
-        os.remove(file_1)
-
-    if os.path.exists(file_2):
-        os.remove(file_2)
-
-    pd.DataFrame(columns=['PHRASE', 'SUGGESTION']).to_csv(file_1, index=False, sep=';')
-
-    depth = input('Укажите грубину рекурсии (нажмите enter для установки значения по умолчанию): ')
-    if depth != '':
-        sys.setrecursionlimit(int(depth))
-
-    passes = input('Укажите количество проходов рекурсии (нажмите enter если хотите ждать завершения рекурсии): ')
-    print('Обратите внимание, что стек рекурсии может переполниться раньше, чем произойдет последний проход!\n')
-
-    i = 1
-    while True:
-        final_dict = {}
-
-        if passes != '':
-            if i > int(passes):
-                break
-
         print(f'\rПроход № {i}', end='')
         i += 1
 
-        try:
-            answer = find_suggestions(phrases, final_dict)
-        except RuntimeError:
-            print('\nГлубина рекурсии превышена!')
-            break
+        new_phrases = phrases
+        suggestion_list = []
 
-        answer = {key: value for key, value in answer.items() if bool(value)}
+        for phrase in phrases:
+            if phrase in all_phrases:
+                continue
 
-        df = pd.DataFrame({'PHRASE': answer.keys(), 'SUGGESTION': answer.values()})
-        values = [x for l in [i.split(', ') for i in answer.values()] for x in l]
+            else:
+                url = f'http://clients1.google.com/complete/search?output=toolbar&num=100&hl=en&pq=suggestqueries.google.com&cp=1&q={phrase}'
+                page = requests.get(url)
+                soup = BeautifulSoup(page.text, 'html.parser')
 
-        if values == phrases:
-            break
+                for tag in soup.find_all('suggestion'):
+                    suggestion_list.append({'PHRASE': phrase, 'SUGGESTION': tag['data']})
         else:
-            phrases = values
+            df = pd.DataFrame(suggestion_list)
+            df = df.loc[df['PHRASE'] != df['SUGGESTION']]
+            df.to_csv(file_1, index=False, header=False, mode='a', sep=';')
 
-        df.to_csv(file_1, index=False, header=False, mode='a', sep=';')
-        with open(file_2, 'a') as f:
-            f.write('\n'.join(values))
+            all_phrases.extend(phrases)
+            phrases = df['SUGGESTION'].to_list()
 
-    df = pd.read_csv(file_1, sep=';')
-    list_of_values = [i.split(', ') for i in df['SUGGESTION'].to_list()]
+df = pd.read_csv(file_1, sep=';')
+df.drop_duplicates(keep='last').to_csv(file_1, index=False, sep=';')  # удаление дубликатов
 
-    values = [x for l in list_of_values for x in l]
-    df.drop_duplicates(subset='PHRASE', keep='last').to_csv(file_1, index=False, sep=';')  # удаление дубликатов
+with open(file_2, 'w') as f:
+    f.write('\n'.join(df['SUGGESTION'].to_list()))
 
-    with open(file_2, 'w') as f:
-        f.write('\n'.join(set(values)))
-
-    print('\nDONE!')
+print('\nDONE!')
